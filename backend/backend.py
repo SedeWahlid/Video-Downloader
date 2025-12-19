a384c0b965d21e936c6c46faa2961b2be6f269ce
Wahlid Sedeghi <Sede.wahlid@gmail.com>
2025-12-19 19:52:56 +0100

    changing the logic of file searching within the backend. Using glob to search the file. Adding glob to the requirements.

--- backend/backend.py
+++ backend/backend.py
@@ -3,6 +3,7 @@ from fastapi.responses import FileResponse
 import yt_dlp as yt
 import uuid
 import os 
+import glob
 
 # -- VIDEO/AUDIO DOWNLOADER -- 
 
@@ -21,23 +22,17 @@ def download_video(url:str,download_type:str, background_tasks : fp.BackgroundTa
     # based on users download type we set the options for the download on video only | audio only | video and audio 
     try: 
         if download_type == "both":
-            target_ext = "mp4"
-            mime_type = "video/mp4"
             option = {
                 'format': 'bestvideo+bestaudio/best',
                 'merge_output_format': 'mp4',
                 'outtmpl': f'downloads/{file_id}.%(ext)s'}
         elif download_type == "audio only":
-            target_ext = "mp3"
-            mime_type = "audio/mpeg"
             option = {
                 'format': 'bestaudio/best',
                 'postprocessors': [{'key': 'FFmpegExtractAudio','preferredcodec': 'mp3','preferredquality': '192',}],
                 'outtmpl': f'downloads/{file_id}.%(ext)s'
                 }
         elif download_type == "video only":
-            target_ext = "mp4"
-            mime_type = "video/mp4"
             option = {
                 'format': 'bestvideo/best',
                 'merge_output_format': 'mp4',
@@ -46,14 +41,29 @@ def download_video(url:str,download_type:str, background_tasks : fp.BackgroundTa
         os.makedirs("downloads", exist_ok=True) # making sure that directory exists for the output files 
         
         with yt.YoutubeDL(option) as ytdl:
-            infos = ytdl.extract_info(url, download= False) # extracting the infos about the video
-            filename_path = f"downloads/{file_id}.{target_ext}" # exact filename path
             ytdl.download([url]) 
+            
+            files = glob.glob(f"downloads/{file_id}*")
+            filename_path = files[0]
+            
+            # get the actual extension found 
+            target_ext = filename_path.split('.')[-1]
+            infos = ytdl.extract_info(url, download=False)
             
             # cleaning filename for user
             title = infos.get('title', 'download')
             safe_filename = "".join([c for c in title if c.isalpha() or c.isdigit() or c==' ']).rstrip()
             user_filename = f"{safe_filename}.{target_ext}"
+            
+            # checking if file is within the server
+            if not user_filename:
+                raise(Exception("Download successful but file not found within server."))
+            
+            if target_ext == "mp3":
+                mime_type = "audio/mpeg"
+            else:
+                mime_type = "video/mp4"
+            
             background_tasks.add_task(cleanup, filename_path) # deleting file after response to not cause server memory overload
             
             return FileResponse(path=filename_path,filename= user_filename, media_type = mime_type)
